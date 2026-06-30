086bd97a11a8591907e05d281301befbf1cdb171
Ina The Cat <bytero1919@gmail.com>
2026-06-30 11:33:25 -0600

    i didnt even tested it, i js wrote it

--- source/game/Stage.hx
+++ source/game/Stage.hx
@@ -2,15 +2,21 @@ package game;
 
 import Sys;
 import backend.system.ANSI;
+import flixel.addons.display.FlxBackdrop;
+import flixel.util.FlxAxes;
+// import flixel.util.typeLimit.OneOfFour;
 import flixel.util.typeLimit.OneOfTwo;
+import openfl.display.BlendMode;
+
+using StringTools;
 
 enum abstract StageObjectType(String)
 {
-    var Sprite = "sprite";
-    var Animated = "animated";
-    var Text = "text";
-    var Backdrop = "backdrop";
-    var Tilemap = "tilemap";
+	var Sprite = 'sprite';
+	var Animated = 'animated';
+	var Text = 'text';
+	var Backdrop = 'backdrop';
+	var Solid = 'solid';
 }
 
 typedef ObjectProperties = {
@@ -44,17 +50,26 @@ typedef ObjectProperties = {
 
     var blend:Null<String>;
 
-    var color:Null<Int>;
+	var color:Null<String>;
 
     var originX:Null<Float>;
     var originY:Null<Float>;
 
-    // Backdrop shii
+	/**
+	 * for backdrops muejejjeje
+	**/
     var repeatX:Null<Bool>;
     var repeatY:Null<Bool>;
 
     var spacingX:Null<Float>;
     var spacingY:Null<Float>;
+	/**
+	 * for Solid thangs... ye, only the size
+	**/
+	var sizeX:Null<Int>;
+
+	var sizeY:Null<Int>;
+	var size:Null<Array<Float>>;
 }
 
 typedef StageObjects = {
@@ -85,21 +100,26 @@ class Stage {
 
 	public var stageObjects:Map<String, FlxSprite> = [];
 
-    public function new() {}
+	public var path:String = '';
+	public var name:String = '';
 
-    public function startStage(jsonPath:String) {
-        if (!Paths.exists('data/stages/$jsonPath.json')){
-            Logs.send('${ANSI.coloredType('$jsonPath', 0xFF0055FF)} was\'nt found', 'Error');
+	public function new() {} // miku needed to win, si, si me arde, y un chingo
+
+	public function startStage(jsonName:String):Void
+	{
+		if (!Paths.exists('data/stages/$jsonName.json'))
+		{
+			Logs.send('${ANSI.coloredType('$jsonName', 0xFF0055FF)} was\'nt found', 'Error');
             JSON = null;
             return;
         }
 
-        JSON = CoolUtil.parseJson('data/stages/$jsonPath');
+		JSON = CoolUtil.parseJson('data/stages/$jsonName');
 
 		setStage();
 	}
 
-	private function setStage()
+	private function setStage():Void
 	{
 		if (JSON == null)
 		{
@@ -107,8 +127,142 @@ class Stage {
 			return;
 		}
 
-		var sprite:FlxSprite;
+		path = JSON.path ?? '';
+		name = JSON.name ?? '';
 
-		//
+		for (e in JSON.objects)
+		{
+			var sprite:OneOfTwo<FlxSprite, FlxBackdrop>;
+
+			var type:String = '';
+
+			switch (e.type)
+			{
+				case Sprite, null:
+					sprite = new FlxSprite(e.properties.pos[0] ?? 0, e.properties.pos[1] ?? 0).loadGraphic(Paths.image(e.sprite));
+					type = 'sprite';
+
+				case Backdrop:
+					var repeats:FlxAxes = XY;
+
+					if (e.properties.repeatX)
+						repeats = X;
+					if (e.properties.repeatY)
+						repeats = Y;
+
+					sprite = new FlxBackdrop(Paths.image(e.sprite), repeats ?? XY, e.properties.spacingX ?? 0, e.properties.spacingY ?? 0);
+					type = 'backdrop';
+
+				case Solid:
+					sprite = new FlxSprite(e.properties.pos[0] ?? 0, e.properties.pos[1] ?? 0);
+					graphicSize(sprite);
+					type = 'solid';
+
+				default:
+					sprite = new FlxSprite(e.properties.pos[0] ?? 0, e.properties.pos[1] ?? 0).loadGraphic(Paths.image(e.sprite));
+					type = 'sprite';
+			}
+
+			sprite.alpha = e.properties.alpha ?? 1;
+			sprite.angle = e.properties.angle ?? 0;
+			sprite.flipX = e.properties.flipX ?? false;
+			sprite.flipY = e.properties.flipY ?? false;
+			sprite.antialiasing = e.properties.antialiasing ?? false; // Options.antialiasing depois
+
+			setColor(sprite, e.properties.color ?? '0xFFFFFFFF');
+			setAcceleration(sprite);
+			setVelocity(sprite);
+			setScale(sprite, e.properties.updateHitbox ?? false);
+		}
+	}
+
+	private function setColor(sprite:OneOfTwo<FlxSprite, FlxBackdrop>, requested:String):Void
+	{
+		for (e in JSON.objects)
+			if (e.color == null)
+				return;
+
+		final REG = ~/^(#|0[xX])([0-9A-Fa-f]{6}|[0-9A-Fa-f]{8})$/;
+
+		if (!REG.match(requested))
+		{
+			Logs.send('Invalid color [$requested]', 'Error');
+			return;
+		}
+
+		var color = requested;
+
+		if (color.startsWith('#'))
+			color = '0x' + color.substr(1);
+
+		sprite.color = Std.parseInt(color);
+	}
+
+	private function graphicSize(sprite:OneOfTwo<FlxSprite, FlxBackdrop>)
+	{
+		for (e in JSON.objects)
+		{
+			if (e.properties.size == null && e.properties.sizeX == null && e.properties.sizeY == null)
+				return;
+
+			if (e.properties.size == null && (e.properties.sizeX != null || e.properties.sizeY != null))
+				sprite.makeGraphic(e.properties.sizeX ?? 25, e.properties.sizeY ?? 25);
+			else
+				sprite.makeGraphic(e.properties.size[0] ?? 25, e.properties.size[1] ?? 25);
+		}
+	}
+
+	private function setAcceleration(sprite:OneOfTwo<FlxSprite, FlxBackdrop>):Void
+	{
+		for (e in JSON.objects)
+		{
+			if (e.properties.acceleration == null && e.properties.accelerationX == null && e.properties.accelerationY == null)
+				return;
+
+			if (e.properties.acceleration == null && (e.properties.accelerationX != null || e.properties.accelerationY != null))
+			{
+				sprite.acceleration.x = e.properties.accelerationX ?? 0;
+				sprite.acceleration.y = e.properties.accelerationY ?? 0;
+			}
+			else
+				sprite.acceleration.set(e.properties.acceleration[0] ?? 0, e.properties.acceleration[1] ?? 0);
+		}
+	}
+
+	private function setVelocity(sprite:OneOfTwo<FlxSprite, FlxBackdrop>):Void
+	{
+		for (e in JSON.objects)
+		{
+			if (e.properties.velocity == null && e.properties.velocityX == null && e.properties.velocityY == null)
+				return;
+
+			if (e.properties.velocity == null && (e.properties.velocityX != null || e.properties.velocityY != null))
+			{
+				sprite.velocity.x = e.properties.velocityX ?? 0;
+				sprite.velocity.y = e.properties.velocityY ?? 0;
+			}
+			else
+				sprite.velocity.set(e.properties.velocity[0] ?? 0, e.properties.velocity[1] ?? 0);
+		}
+	}
+
+	private function setScale(sprite:OneOfTwo<FlxSprite, FlxBackdrop>, updateHitbox:Null<Bool>):Void
+	{
+		for (e in JSON.objects)
+		{
+			if (e.properties.scale == null && e.properties.scaleyX == null && e.properties.scaleY == null)
+				return;
+
+			if (e.properties.scale == null && (e.properties.scaleX != null || e.properties.scaleY != null))
+			{
+				sprite.scale.x = e.properties.scaleX ?? 0;
+				sprite.scale.y = e.properties.scaleY ?? 0;
+			}
+			else
+				sprite.scale.set(e.properties.scale[0] ?? 0, e.properties.scale[1] ?? 0);
+
+			if (updateHitbox)
+				sprite.updateHitbox();
+		}
     }
 }
